2ed601ecc011c09366167b02a565b9714b173fcb
Gerard <jonesgerard25@gmail.com>
2019-07-01 07:41:55 -0400

    modified:   Invest/core/Core.py
    Took 25 minutes

--- Invest/core/EC_Share_Index.py
+++ Invest/core/EC_Share_Index.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup as Soup
 from pathlib import Path
 from Invest.core.WebAccess import parse_website
 import time
+import  sys
 
 save_location = Path(Path.home(), "Desktop","EC  index")
 
@@ -36,7 +37,7 @@ def EC_Share_Index(web_page="http://www.ecseonline.com/ecse_index.php"):
     #  -----
     filename = security_name + ".csv"
     file = Path(save_location, filename)
-    print("Location of Flile ---> ", file)
+    print("Location of File ---> ", file)
 
     if file.is_file():
         print("T/F Does the file already exist? ---> ", 'True')
@@ -48,18 +49,35 @@ def EC_Share_Index(web_page="http://www.ecseonline.com/ecse_index.php"):
             string = ''.join(last_entry)
             last_date = string[:len(string) - 7].strip()
 
+            # testing material 
+            # with open("newdoc.txt","w") as nd:
+            #     nd.write(a)
+            #     
+            # with open("newdoc.txt","a") as nd:
+            #    var1 =f.read()
+            #     nd.write(f"\n1{var1}")
+            #     
+
+            # with open("newdoc.txt","a") as nd:
+            #     nd.write(f"\n2{content}")
+            #     sys.exit()
+            
+
             #  print("last_date",last_date)
             #  print("Date", date)
             if last_date.strip() == current_date.strip():
                 # print("\n", "An entry for", current_date.strip(), "was already made")
-                print("\n", "An entry for", last_date.strip(), "was already made")
-                time.sleep(5)
-                return
+                print(f"\n An entry for {last_date.strip()} was already made  :P :) ")
+                print( f" \n {market_value}  \n ")
+                print("\n  thanks for the time byee :P : \n")
+                time.sleep(4)
+            return
 
         doc = open(file, "a")
         doc.write(current_date + "," + market_value + "\n")
     else:
-        print("false")
+        #create a new file
+        print(f"false \n now creating a new file {None} ")
         doc = open(file, "a")
         title = "Security Name: " + str(security_name) + "\n"
         headers = "Date, Market Value\n"
@@ -69,7 +87,7 @@ def EC_Share_Index(web_page="http://www.ecseonline.com/ecse_index.php"):
         doc.write(current_date + "," + market_value + "\n")
 
     doc.close()
-    print("\n", "A New Entry for", current_date.strip(), "Made ")
+    print("\n", "A New Entry for", current_date.strip(), "Was Made :P :) ")
     time.sleep(9)
     return
 
